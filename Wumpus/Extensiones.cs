778112ff7224a64d8651e3a5c358b5763b379c28
Francisco Inostroza Núñez <franciscoinostroza@outlook.com>
2021-10-31 06:43:27 -0300

    Clases de mapa, jugador agregados.

---
+++ Wumpus/Extensiones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wumpus
+{
+    static class Extensiones {
+        public static bool En<T>(this T elemento, params T[] elementos) {
+            if (elemento == null) throw new ArgumentNullException("Elemento");
+            return elementos.Contains(elemento);
+        }
+    }
+}
---
+++ Wumpus/Jugador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wumpus
+{
+    class Jugador {
+        private bool flecha { get; set; }
+        private int puntaje { get; set; }
+        public Jugador () {
+            this.flecha = true;
+            this.puntaje = 0;
+        }
+        public void lanzarFlecha () {
+            this.flecha = false;
+        }
+    }
+}
